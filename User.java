eff5daf8a881906ccd753afd49cc37aab0615bdc
jianglei <1058260449@qq.com>
2020-03-27 11:41:35 +0800

    2此修改

--- User.java
+++ User.java
@@ -2,9 +2,9 @@
 * @Author: jianglei
 * @Date:   2020-03-27 11:36:48
 * @Last Modified by:   jianglei
-* @Last Modified time: 2020-03-27 11:37:59
+* @Last Modified time: 2020-03-27 11:39:22
 */
 
 public class User {
-    
+  private int id;  
 }
